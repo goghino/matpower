815b8e43c7cf74b0e2952ac21dcb043bb748495f
Juraj Kardos <juraj.kardos@usi.ch>
2017-07-26 14:15:56 +0200

    code inspecting the matrices

--- main/SCOPF.m
+++ main/SCOPF.m
@@ -17,12 +17,23 @@ ngen = size(mpc.gen, 1);
 
 
 %%
-cond_KKT = zeros(nbrch,1);
-cond_hess = zeros(nbrch,2);
-cond_block = zeros(nbrch,2);
 
-%for i = 1:nbrch
-for i = [2 3 5 6 8 9] %case9 branches that do not leave isolated bus if cut
+%list of case9 branches that do not leave isolated bus if cut
+contingencies = [2 3 5 6 8 9];
+
+%condition number for the full IPOPT system
+cond_KKT = zeros(nbrch,1);
+%condition number of the small diagonal blocks before reordering
+cond_hess = zeros(nbrch,2);
+%condition number of the big diagonal blocks after reordering
+cond_block = zeros(nbrch,2);
+cond_pblock = zeros(nbrch,2);
+%condition of part of the diagonal block from cond_block
+cond_bblock1 = zeros(nbrch,2); %SC w.r.t smaller block
+cond_bblock2 = zeros(nbrch,2); %SC w.r.t bigger block
+cond_bblock3 = zeros(nbrch,2); %SC w.r.t bigger block
+
+for i = contingencies
     
     % specify list of branch contingencies, equals to OPF if empty
     cont = [i]'; 
@@ -31,8 +42,16 @@ for i = [2 3 5 6 8 9] %case9 branches that do not leave isolated bus if cut
     %runopf(mpc, mpopt);
     runscopf(mpc, cont, mpopt);
 
-    % inspect IPOPT hessian
-    A = readcsr('mat-ipopt_004-01.iajaa', 0, 1); 
+    % read-in IPOPT hessian and permute it
+    name = ls('mat-ipopt_004-*');
+    starts = strfind(name,'mat-ipopt'); %check if there is more mat-ipopt_004s
+    if size(starts,2) > 1
+       name = name(1:starts(2)-2); %pick first file 004_
+    else
+        name = name(1:end-1); % delete trailing ' '
+    end
+    
+    A = readcsr(name, 0, 1); 
     [P Pinv] = KKTpermute(nbus, nbrch, ngen, ns);
     AP = A(P,P'); 
     
@@ -44,14 +63,44 @@ for i = [2 3 5 6 8 9] %case9 branches that do not leave isolated bus if cut
 
     %local hessians wrt primal variables for nominal and contingency
     nH = 2*nbus;
-    cond_hess(i,1) = cond(full(A(1:nH,1:nH)));
-    cond_hess(i,2) = cond(full(A(nH+(1:nH),nH+(1:nH))));
+    for j = 1:ns
+        cond_hess(i,j) = cond(full(A((j-1)*nH+(1:nH),(j-1)*nH+(1:nH))));
+    end
     
     %diagonal blocks after permutation
     npart = 2*nbus + 2*nbus + 2*nbrch + 2*nbrch;
     nppart = 2*nbus + 2*nbus + 2*nbrch;
-    cond_block(i,1) = cond(full(AP(1:npart,1:npart)));
-    cond_block(i,2) = cond(full(AP(npart+(1:npart),npart+(1:npart))));
+    for j = 1:ns
+        block = AP((j-1)*npart+(1:npart),(j-1)*npart+(1:npart));
+        cond_block(i,j) = cond(full(block));
+        
+        %preprocess block
+        d = abs(diag(block));
+        idx = find(d < 1e-12);
+        d(idx) = 1;
+        S = diag(1./sqrt(d));
+        cond_pblock(i,j) = cond(full(S*block*S));
+        
+        %break down the block into more blocks
+        block11 = block(1:nppart,1:nppart);
+        block22 = block(nppart+(1:2*nbrch),nppart+(1:2*nbrch));
+        B = block(nppart+(1:2*nbrch),1:nppart);
+        BT = block(1:nppart,nppart+(1:2*nbrch));
+        
+        SC = block11 - BT*(block22\B); %SC w.r.t block 1,1
+        
+        cond_bblock1(i,j) = cond(full(block11));
+        cond_bblock2(i,j) = cond(full(SC));
+        
+        %do 3-rd level of SC
+        block11 = SC(1:18,1:18);
+        block22 = SC(19:end, 19:end);
+        BT = SC(1:18, 19:end);
+        B = SC(19:end, 1:18);
+        
+        SC = block22 - B*(block11\BT);
+        cond_bblock3(i,j) = cond(full(SC));
+    end
     
     %remove IPOPT matrices
     delete *.iajaa;
@@ -60,6 +109,9 @@ end
 cond_KKT
 cond_hess
 cond_block
+cond_pblock
+cond_bblock1
+cond_bblock2
 
 %isolating bus by contingency creates singular Cb, zero on diagonal
 % -> singular local hessian and fucked up condition numbers!!!
@@ -100,6 +152,3 @@ cond_block
 %   constant of if there are missing entries corresponding to a contingency
 %3. generate tests for matpower_cpp, keep in mind that the entries are kept
 %   there but they have zero value.
-%4. could it be that Jac of constraints is singular? reason could be same
-%   branch flow limits and very similar voltages on the attached branches
-%   so that rows becomes almost the same
