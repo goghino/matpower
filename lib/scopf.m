f029e0e58e779e3a5c6ffa79205821a6541c3a14
Juraj Kardos <juraj.kardos@usi.ch>
2018-01-16 11:17:52 +0100

    adding one extra level of contingency filter - OPF feasibility with reduced network

--- lib/scopf.m
+++ lib/scopf.m
@@ -75,6 +75,11 @@ cont_filt = setdiff(cont_filt, duplicates);
 critical = findQgCritical(mpc, cont_filt, 0); %no violation of QG allowed
 cont_filt = setdiff(cont_filt, critical);
 
+%%-- remove lines that make OPF problem infeasible when removed
+cont_filt = findOPFfeasible(mpc, cont_filt);
+
+dlmwrite('cont_filt.txt',cont_filt-1);
+
 cont = [-1; cont_filt(1:min(N,length(cont_filt)))]; %add nominal case and N contingencies
 ns = length(cont);
 
