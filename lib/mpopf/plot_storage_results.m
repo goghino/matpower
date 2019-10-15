41c5b24f1c4d03e4f34d901e4afd7029c4bd2651
Juraj Kardos <juraj.kardos@usi.ch>
2019-10-15 13:38:44 +0200

    adding analysis of the flexibility results

--- lib/mpopf/plot_storage_results.m
+++ lib/mpopf/plot_storage_results.m
@@ -6,15 +6,15 @@ function plot_storage_results(mpc_solution)
     N = mpc_solution.horizon;
     Pgen_discharge = reshape(mpc_solution.gen(mpc_solution.id_gen_storages_discharge,PG),[nstorage, N ]);
     Pgen_charge    = reshape(mpc_solution.gen(mpc_solution.id_gen_storages_charge   ,PG),[nstorage, N ]);
+    max_product_charge_discharge_MWMW = max(max(abs(Pgen_discharge.*Pgen_charge)))
+    
+    
     Pgen_storage = Pgen_discharge+Pgen_charge;
     Pgen_storage_max = repmat(mpc_solution.P_storage_max_MW,[1,N]);
     Pgen_storage_min = repmat(mpc_solution.P_storage_min_MW,[1,N]);
 
     E_storage = [mpc_solution.E_storage_init_MWh, repmat(mpc_solution.E_storage_init_MWh,[1,N]) - cumsum(Pgen_discharge./repmat(mpc_solution.c_discharge,[1,N]) + Pgen_charge.*repmat(mpc_solution.c_charge,[1,N]),2)];
     E_storage_max = repmat(mpc_solution.E_storage_max_MWh,[1,N+1]);
-
-
-    max_product_charge_discharge_MWMW = max(max(abs(Pgen_discharge.*Pgen_charge)))
 
     %set matching colors for discharge and the corresponding maximum
     map = get(gca,'ColorOrder');
@@ -29,7 +29,7 @@ function plot_storage_results(mpc_solution)
     subplot(2,1,1)
     stairs(Pgen_discharge', 'LineWidth',5);
     hold on;
-    stairs(0:N, [Pgen_storage_max Pgen_storage_max(:,end)  ]', '--', 'LineWidth',3);
+    stairs(1:N, [Pgen_storage_max ]', '--', 'LineWidth',3);
     grid on
     title('Storage network injection - discharging')
     xlabel('t [hours]')
@@ -38,7 +38,7 @@ function plot_storage_results(mpc_solution)
     subplot(2,1,2)
     stairs(Pgen_charge', 'LineWidth',5);
     hold on;
-    stairs(0:N, [Pgen_storage_min Pgen_storage_min(:,end)  ]', '--', 'LineWidth',3);
+    stairs(1:N, [Pgen_storage_min]', '--', 'LineWidth',3);
     grid on
     title('Storage network injection - charging')
     xlabel('t [hours]')
@@ -51,10 +51,10 @@ function plot_storage_results(mpc_solution)
     subplot(2,1,1)
     stairs( [Pgen_storage ]', 'LineWidth',5);
     hold on;
-    stairs(0:N, [Pgen_storage_max Pgen_storage_max(:,end)  ]', '--', 'LineWidth',3);
-    stairs(0:N, [Pgen_storage_min Pgen_storage_min(:,end)  ]', '--', 'LineWidth',3);
+    stairs(1:N, [Pgen_storage_max]', '--', 'LineWidth',3);
+    stairs(1:N, [Pgen_storage_min]', '--', 'LineWidth',3);
     grid on
-    title('Storage network injection power : trajectory (solid) and maximum (dashed)')
+    title('Individual storage network injection power : trajectory (solid) and maximum (dashed)')
     xlabel('t [hours]')
     ylabel('Storage power [MW]')
     set(gca,'FontSize',20);
@@ -63,7 +63,7 @@ function plot_storage_results(mpc_solution)
     hold on;
     plot(0:N, E_storage_max','--', 'LineWidth',3);
     grid on
-    title('Storage energy level: trajectory (solid) and maximum (dashed)')
+    title('Individual storage energy level: trajectory (solid) and maximum (dashed)')
     xlabel('t [hours]')
     ylabel('Storage energy [MWh]')
     set(gca,'FontSize',20);
@@ -76,7 +76,7 @@ function plot_storage_results(mpc_solution)
     stairs(0:N, sum([Pgen_storage_max Pgen_storage_max(:,end)  ])', 'b--');
     stairs(0:N, sum([Pgen_storage_min Pgen_storage_min(:,end)  ])', 'b--');
     grid on
-    title('Total storage network injection power : trajectory (solid) and maximum (dashed)')
+    title('Cumulative storage network injection power : trajectory (solid) and maximum (dashed)')
     xlabel('t [hours]')
     ylabel('Storage power [MW]')
     set(gca,'FontSize',20);
@@ -85,7 +85,7 @@ function plot_storage_results(mpc_solution)
     hold on;
     plot(0:N, sum(E_storage_max)','b--');
     grid on
-    title('Total storage energy level: trajectory (solid) and maximum (dashed)')
+    title('Cumulative storage energy level: trajectory (solid) and maximum (dashed)')
     xlabel('t [hours]')
     ylabel('Storage energy [MWh]')
     set(gca,'FontSize',20);
